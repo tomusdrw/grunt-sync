5674c78910c6331725955c9be75b7e803d0ad1ca
xzyfer <xzyfer@gmail.com>
2013-12-16 12:38:43 +1100

    Fix broken destination paths with expanded mappings
    When using expanding mappings the `file.dest` refers a destination file name, rather than the usual destination directory which was previously assumed.
    
    This results is weirdly nested destination directories.

--- tasks/sync.js
+++ tasks/sync.js
@@ -51,7 +51,13 @@ module.exports = function(grunt) {
     promise.all(this.files.map(function(fileDef) {
       var cwd = fileDef.cwd ? fileDef.cwd : '.';
       return promise.all(fileDef.src.map(function(src){
-        return processPair(path.join(cwd, src), path.join(fileDef.dest, src));
+        var dest = path.join(fileDef.dest, src);
+        // when using expanded mapping dest is the destination file
+        // not the destination folder
+        if(fileDef.orig.expand) {
+          dest = fileDef.dest;
+        }
+        return processPair(path.join(cwd, src), dest);
       }));      
     })).then(function(promises) {
       promise.all(promises).then(done);
